497fa7c843ee57dd23e0fe6c3cba13b8d70d43c4
Ford <freeforalls@hotmail.com>
2021-08-31 08:53:15 +0700

    create api individualInformation

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 import express from "express";
-const app = express()
-const port = 3000
+import { individualInformationMap } from "./modules/individualInformation.js";
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+
+app.get("/individual-information/:email", (req, res) => {
+  let email = req.params.email;
+  let individualInformation = individualInformationMap.get(email);
+  res.json(individualInformation);
+});
