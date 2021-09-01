272c8b88856e2ba8879a8a23e7cd0413288cdaff
Fah <tadsika@odds.team>
2021-09-01 08:39:49 +0700

    add index.js and monthlyPayment.js

---
+++ modules/index.js
@@ -0,0 +1,7 @@
+import {individualInformation}  from "./individualInformation.js";
+import {workingRecord} from "./workingRecords.js";
+import { monthlyPayment } from "./monthlyPayment.js";
+
+function createmonthlyPayment (dailyRate,workingDay)
+return dailyRate * workingDay
+
---
+++ modules/monthlyPayment.js
@@ -0,0 +1,26 @@
+class monthlyPayment {
+    constructor(
+        name,
+        surname,
+        email,
+        accountNumber,
+        telephoneNumber,
+        workingDay,
+        transferAmount,
+        submittedDate,
+        remark
+    )
+    
+    {
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.accountNumber = accountNumber;
+        this.telephoneNumber = telephoneNumber;
+        this.workingDay = workingDay;
+        this.transferAmount = transferAmount;
+        this.submittedDate = submittedDate;
+        this.remark = remark;
+    }
+}
+export {monthlyPayment}
