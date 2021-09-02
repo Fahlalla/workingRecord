0218665f5cc567100b4b77d27a583a74d13509d1
Ford <freeforalls@hotmail.com>
2021-09-02 08:52:02 +0700

    fix api

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { individualInformationMap } from "./models/individualInformation.js";
 import { workingRecordMap } from "./models/workingRecords.js";
-import { createMonthlyPayment } from "./models/index.js";
+import { transferAmount } from "./models/index.js";
 
 const app = express();
 const port = 3000;
@@ -25,6 +25,6 @@ app.get("/working-records/:email", (req, res) => {
 });
 
 app.get("/monthly-payment/:email", (req, res) => {
-  let monthlyPayment = createMonthlyPayment(req.params.email);
+  let monthlyPayment = transferAmount(req.params.email);
   res.json(monthlyPayment);
 });
