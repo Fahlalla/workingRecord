36e9eb4cfc29e83d4122d7bc0fbb31e788a9fa94
Ford <freeforalls@hotmail.com>
2021-09-02 08:32:55 +0700

    add missing variable

--- models/monthlyPayment.js
+++ models/monthlyPayment.js
@@ -2,9 +2,11 @@ class monthlyPayment {
     constructor(
         name,
         surname,
+        nickName,
         email,
         accountNumber,
         telephoneNumber,
+        dailyRate,
         workingDay,
         transferAmount,
         submittedDate,
@@ -14,9 +16,11 @@ class monthlyPayment {
     {
         this.name = name;
         this.surname = surname;
+        this.nickName = nickName;
         this.email = email;
         this.accountNumber = accountNumber;
         this.telephoneNumber = telephoneNumber;
+        this.dailyRate = dailyRate;
         this.workingDay = workingDay;
         this.transferAmount = transferAmount;
         this.submittedDate = submittedDate;
