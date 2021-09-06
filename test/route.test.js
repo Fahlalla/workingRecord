3cfdc904d3bf7695541349177bff67874bc95570
Fah <tadsika@odds.team>
2021-09-06 21:53:32 +0700

    Move api test to route

--- test/route.test.js
+++ test/route.test.js
@@ -13,4 +13,18 @@ describe("Post Endpoints", () => {
     const res = await request(app).get("/").send();
     expect(res.statusCode).toEqual(200);
   });
+
+  test ('api path / should be return hello world',async() => {
+
+    const res = await request(app).get('/')
+   expect(res.statusCode).toEqual(200)
+   expect(res.text).toBe("Hello World!")
+  });
+
+  test('api path /individual-information/', async() => {
+    const res = await request(app).get('/individual-information/glock@odds.team')
+    expect(res.statusCode).toEqual(200)
+    expect(res.text).toBe("{\"name\":\"panudet\",\"surname\":\"jitti\",\"nickName\":\"Ford\",\"phoneNumber\":\"1234567890\",\"email\":\"glock@odds.team\",\"site\":\"Saksiam\",\"dailyRate\":\"999\"}")
+  });
 });
+
--- test/sum.test.js
+++ test/sum.test.js
@@ -6,18 +6,9 @@ test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
 
-test ('api path / should be return hello world',async() => {
-  const res = await request(app).get('/')
- expect(res.statusCode).toEqual(200)
- expect(res.text).toBe("Hello World!")
- 
-});
 
 test('api path /individual-information/', async() => {
   const res = await request(app).get('/individual-information/glock@odds.team')
   expect(res.statusCode).toEqual(200)
   expect(res.text).toBe("{\"name\":\"panudet\",\"surname\":\"jitti\",\"nickName\":\"Ford\",\"phoneNumber\":\"1234567890\",\"email\":\"glock@odds.team\",\"site\":\"Saksiam\",\"dailyRate\":\"999\"}")
 });
-//app.get("/999", (req, res) => {
- // res.send("Hello Siri!");
-//});
