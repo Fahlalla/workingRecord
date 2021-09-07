d8f268137800f4bb61894555deea7af38d173a46
Glock Rastarfah <nighttime.google_@hotmail.com>
2021-09-07 08:29:28 +0700

    refactor and remove test/sum.test.js

--- test/sum.test.js
+++ test/sum.test.js
@@ -1,14 +1,5 @@
 import {sum} from './sum.js';
-import {app} from '../app.js';
-import request from 'supertest';
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
-});
-
-
-test('api path /individual-information/', async() => {
-  const res = await request(app).get('/individual-information/glock@odds.team')
-  expect(res.statusCode).toEqual(200)
-  expect(res.text).toBe("{\"name\":\"panudet\",\"surname\":\"jitti\",\"nickName\":\"Ford\",\"phoneNumber\":\"1234567890\",\"email\":\"glock@odds.team\",\"site\":\"Saksiam\",\"dailyRate\":\"999\"}")
 });
