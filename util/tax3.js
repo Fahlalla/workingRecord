9a1fc73fce75fc27b6df3a4572f53a99b3c4872a
Chaya Phuwichit <chaya@hiqfood.com>
2021-09-14 08:03:08 +0700

    [Update] tax3

--- util/tax3.js
+++ util/tax3.js
@@ -10,4 +10,20 @@ function netTransferAmount (monthlySalaryAmount,tax3Amount){
     return monthlySalaryAmount-tax3Amount
 }
 
-export {monthlySalaryAmount,tax3Amount,netTransferAmount}
+export { monthlySalaryAmount,tax3Amount,netTransferAmount}
+
+/*
+function tax3(dailyRate,workingDay){
+    let MonthlySalaryAmount = monthlySalaryAmount (dailyRate,workingDay);
+    let Tax3Amount =  tax3Amount(_monthlySalaryAmount);
+    let TransferAmount =netTransferAmount(MonthlySalaryAmount,Tax3Amount);
+    return {
+        monthlySalaryAmount:MonthlySalaryAmount,
+        tax3Amount:Tax3Amount,
+        netTransferAmount:TransferAmount
+    };
+}
+
+export { tax3,monthlySalaryAmount,tax3Amount,netTransferAmount}
+
+*/
