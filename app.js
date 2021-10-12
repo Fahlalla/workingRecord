acd027759a737ea5fc26a320846ff29245d8ffba
mindpakawat <48022706+mindpakawat@users.noreply.github.com>
2021-10-12 08:22:44 +0700

    Fix app listening

--- app.js
+++ app.js
@@ -7,35 +7,35 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  return res.status(200).send("Hello World!");
+    return res.status(200).send("Hello World!");
 });
 
 app.get("/individual-information/:email", (req, res) => {
-  const email = req.params.email;
-  const result = getIndividualInformationByEmail(email);
+    const email = req.params.email;
+    const result = getIndividualInformationByEmail(email);
 
-  if(result == undefined)
-    return res.status(404).json('Individual Information not found');
-  return res.status(200).json(result);
+    if (result == undefined)
+        return res.status(404).json('Individual Information not found');
+    return res.status(200).json(result);
 });
 
-app.get("/working-records/:email", async (req, res) => {
-  const email = req.params.email;
-  const result = await getWorkingRecordByEmail(email);
+app.get("/working-records/:email", async(req, res) => {
+    const email = req.params.email;
+    const result = await getWorkingRecordByEmail(email);
 
-  if(result == undefined)
-    return res.status(404).json('Working Record not found');
-  return res.status(200).json(result);
+    if (result == undefined)
+        return res.status(404).json('Working Record not found');
+    return res.status(200).json(result);
 });
 
 app.get("/monthly-payment/:email", (req, res) => {
-  const email = req.params.email;
-  let monthlyPayment = transferAmount(email);
-  return res.json(monthlyPayment);
+    const email = req.params.email;
+    let monthlyPayment = transferAmount(email);
+    return res.json(monthlyPayment);
 });
 
-const server = app.listen(port, () => {
-  return console.log(`Example app listening at http://localhost:${port}`);
+const server = app.listen(process.env.PORT || 3000, () => {
+    return console.log(`Example app listening at http://localhost:${port}`);
 });
 
-export {app, server}
+export { app, server }
